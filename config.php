3d66a88f10e31c32a5978f20e93343ef9781fd2f
abdullahAljuhi <95518730+abdullahAljuhi@users.noreply.github.com>
2022-04-01 18:30:24 +0300

    edite app path

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ if(!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 
-define('APP_PATH', realpath(dirname(__FILE__)) . DS . '..');
+define('APP_PATH', realpath(dirname(__FILE__)) . DS );
 define('VIEWS_PATH', APP_PATH . DS . 'views' . DS);
