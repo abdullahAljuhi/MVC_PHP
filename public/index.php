0b9dad678d36bce8f441f624795f5b8069c2e142
abdallah aljuhi <abdallahalju7@gmail.com>
2022-04-01 09:46:46 +0300

    fixed composer autoload does not worke

--- public/index.php
+++ public/index.php
@@ -1,23 +1,26 @@
 <?php
-
+require_once dirname(__DIR__).'/vendor/autoload.php';
 /**
 *
 * main controller
 * php version 8.0
 */
+
+use Core\Router;
+
 // require '../App/Controllers/Posts.php';
 // require '../Core/Router.php';
 // echo 'Requested url="'.$_SERVER["QUERY_STRING"].'"';
 
-spl_autoload_register(function($class){
-    $root =dirname(__DIR__);
-    $file = $root.'/'.str_replace('\\','/',$class).'.php';
-    if(is_readable($file)){
-        require $root . '/'.str_replace('\\','/',$class).'.php';
-    }
-});
+// spl_autoload_register(function($class){
+//     $root =dirname(__DIR__);
+//     $file = $root.'/'.str_replace('\\','/',$class).'.php';
+//     if(is_readable($file)){
+//         require $root . '/'.str_replace('\\','/',$class).'.php';
+//     }
+// })
 
-$router = new Core\Router();
+$router = new Router;
 $router->add('',['controller'=>'Home','action'=>'index']);
 $router->add('{controller}/{action}');
 
